16fc2f82e751230e8fbaa09d231ac8a1de849744
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-13 04:32:28 +0530

    added the colors and other properties

--- styles/theme.js
+++ styles/theme.js
@@ -1,6 +1,22 @@
 const theme = {
-  colors: {},
-  size: {},
+  colors: {
+    black: "#04141A",
+    white: "#F4F4F4",
+    bgLight: "#FAF5E5",
+    bgMedium: "#FCE7D0",
+    bgDark: "#E6C7A8",
+    primary: "#D69D19",
+    primaryLight: "#ECB662",
+    secondary: "#7C7C7C",
+    secondaryLight: "#BCBCBC",
+  },
+  fonts: {
+    primary: "sans-sarif",
+    secondary: "'Made Canvas', 'Times New Roman', Times, serif",
+  },
+  sizes: {
+    frameSize: "1.5rem",
+  },
   responsive: {},
 };
 
