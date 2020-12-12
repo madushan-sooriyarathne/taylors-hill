594c9a60f0b2e5d17df3ed601e29e31d95cda0c9
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-13 01:30:53 +0530

    created _document.js and _app.js

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import { ThemeProvider } from "styled-components";
+import Head from "next/head";
+import theme from "../styles/theme";
+import GlobalStyles from "../styles/globalStyles";
+
+const MainApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Taylors Hill Boutique Hotel</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MainApp;
