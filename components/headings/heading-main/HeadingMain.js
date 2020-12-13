ea8f7d1e09ae3630d84bf253951d0ba6e3e26974
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-13 17:28:13 +0530

    created main heading component

---
+++ components/headings/heading-main/HeadingMain.js
@@ -0,0 +1,7 @@
+import { Heading } from "./HeadingMainStyles";
+
+const HeadingMain = ({ children }) => {
+  return <Heading>{children}</Heading>;
+};
+
+export default HeadingMain;
---
+++ components/headings/heading-main/HeadingMainStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-size: 5rem;
+  font-weight: 900;
+  font-family: ${(props) => props.theme.fonts.secondary};
+  color: ${(props) => (props.color ? props.color : props.theme.colors.white)};
+`;
+
+export { Heading };
