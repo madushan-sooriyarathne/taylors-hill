7000a9b533f039107c867334a18d1612363b0be1
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-15 02:46:59 +0530

    created HeadingSecondary component

---
+++ components/headings/heading-secondary/HeadingSecondary.js
@@ -0,0 +1,7 @@
+import { Heading } from "./HeadingSecondaryStyles";
+
+const HeadingSecondary = ({ children, color }) => (
+  <Heading color={color}>{children}</Heading>
+);
+
+export default HeadingSecondary;
