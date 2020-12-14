7e62d94a3077742ed70802c186219fd7e8c07892
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-15 03:00:14 +0530

    added few section to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import CTADescription from "../components/layout/cta-description/CTADescription";
+import ImageContentSection from "../components/layout/Image-content-section/ImageContentSection";
 import Page from "../components/layout/page/Page";
+import RoomsSection from "../components/layout/rooms-section/RoomsSecton";
 import Slider from "../components/layout/slider/Slider";
+import { ourStory, dining } from "../site-data";
 
 const Home = () => {
   return (
     <Page>
       <Slider />
+      <ImageContentSection image={ourStory.image}>
+        <CTADescription {...ourStory} />
+      </ImageContentSection>
+      <RoomsSection />
+      <ImageContentSection image={dining.image} reversed>
+        <CTADescription {...dining} />
+      </ImageContentSection>
     </Page>
   );
 };
