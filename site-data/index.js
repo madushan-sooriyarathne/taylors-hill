96f80d77b9b7e8c5ae5ea2bbb57e5acdde58996b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-14 20:22:22 +0530

    created a sperate file to hold site meta data

---
+++ site-data/index.js
@@ -0,0 +1,11 @@
+const navLinks = [
+  { id: 0, name: "Home", route: "/" },
+  { id: 1, name: "Story", route: "/story" },
+  { id: 2, name: "Accommodation", route: "/accommodation" },
+  { id: 3, name: "Experience", route: "/experience" },
+  { id: 4, name: "Offers", route: "/offers" },
+  { id: 5, name: "Wellness", route: "/wellness" },
+  { id: 6, name: "Contact", route: "/contact" },
+];
+
+export { navLinks };
