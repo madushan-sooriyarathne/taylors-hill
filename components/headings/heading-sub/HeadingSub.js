e80f1ae76fe1c86b6f59dc0f18832e897179b6bc
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-15 02:46:38 +0530

    created HeadignSub component

---
+++ components/headings/heading-sub/HeadingSub.js
@@ -0,0 +1,7 @@
+import { Heading } from "./HeadingSubStyles";
+
+const HeadingSub = ({ color, children }) => (
+  <Heading color={color}>{children}</Heading>
+);
+
+export default HeadingSub;
---
+++ components/headings/heading-sub/HeadingSubStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Heading = styled.h3`
+  font-size: 1.8rem;
+  font-weight: 300;
+  font-family: ${(props) => props.theme.fonts.primary};
+  letter-spacing: 1px;
+  color: ${(props) =>
+    props.color ? props.color : props.theme.colors.secondary};
+  text-align: center;
+`;
+
+export { Heading };
