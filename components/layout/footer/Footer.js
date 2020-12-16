a54da950475c5a104a7af86da48eb06ca8cb9c6f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-17 01:31:47 +0530

    created the footer

---
+++ components/layout/footer/Footer.js
@@ -0,0 +1,124 @@
+import Link from "next/link";
+import {
+  Container,
+  Logo,
+  Newsletter,
+  NewsletterTitle,
+  InputWrapper,
+  InputFieldWrapper,
+  NewsletterInput,
+  NewsletterButton,
+  ContactWrapper,
+  ContactGroup,
+  ContactItem,
+  LinkWrapper,
+  SocialMediaGroup,
+  SocialIcon,
+  LinkGroup,
+  LinkItem,
+  CopyrightWrapper,
+  CopyrightItem,
+} from "./FooterStyles";
+
+const socials = [
+  {
+    id: 0,
+    name: "Facebook",
+    logo: "facebook",
+    url: "https://www.facebook.com/taylorshillsrilanka",
+  },
+  {
+    id: 1,
+    name: "Instagram",
+    logo: "instagram",
+    url: "https://www.instagram.com/taylorshillboutiquehotel",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    logo: "linkedin",
+    url: "https://www.linkedin.com/company/taylors-hill-boutique-hotel",
+  },
+];
+
+const footerLinks = [
+  { id: 0, name: "Contact", route: "/contact" },
+  { id: 1, name: "Privacy", route: "/privacy" },
+  {
+    id: 2,
+    name: "Payment & Cancellations",
+    route: "/payment-cancellation-policy",
+  },
+  { id: 3, name: "Site Map", route: "/site-map" },
+];
+
+const Footer = () => {
+  return (
+    <Container>
+      <Logo
+        src="/static/svg/th-logo.svg"
+        alt="Luxury Boutique Hotel in Sri Lanka"
+      />
+      <Newsletter>
+        <NewsletterTitle>Subscribe to our newsletter</NewsletterTitle>
+        <InputWrapper>
+          <InputFieldWrapper>
+            <NewsletterInput
+              type="email"
+              placeholder="name@email.com"
+              required
+            />
+          </InputFieldWrapper>
+          <NewsletterButton type="submit">
+            Subscribe{" "}
+            <svg>
+              <use xlinkHref="/static/svg/sprites.svg#send"></use>
+            </svg>
+          </NewsletterButton>
+        </InputWrapper>
+      </Newsletter>
+      <ContactWrapper>
+        <ContactGroup>
+          <ContactItem>Pattiyagama Estate, Deltota,</ContactItem>
+          <ContactItem>Kandy, Sri Lanka. 20430</ContactItem>
+        </ContactGroup>
+        <ContactGroup>
+          <ContactItem>Tel: +94 81 527 7788</ContactItem>
+          <ContactItem>Reservations: +94 707 123 678</ContactItem>
+          <ContactItem>Email: reservations@taylorshill.lk</ContactItem>
+        </ContactGroup>
+      </ContactWrapper>
+      <LinkWrapper>
+        <SocialMediaGroup>
+          {socials.map((item) => (
+            <a href={item.url} target="_blank" rel="noopener" key={item.id}>
+              <SocialIcon>
+                <use xlinkHref={`/static/svg/sprites.svg#${item.logo}`}></use>
+              </SocialIcon>
+            </a>
+          ))}
+        </SocialMediaGroup>
+        <LinkGroup>
+          {footerLinks.map((link) => (
+            <Link href={link.route} key={link.id}>
+              <LinkItem>{link.name}</LinkItem>
+            </Link>
+          ))}
+        </LinkGroup>
+      </LinkWrapper>
+      <CopyrightWrapper>
+        <CopyrightItem>
+          Taylors Hill Boutique Hotel {new Date().getFullYear()} ©
+        </CopyrightItem>
+        <CopyrightItem>
+          Handcrafted with ❤️ by{" "}
+          <a href="https://madushan.dev" rel="noopener" target="_blank">
+            Madushan Sooriyarathne
+          </a>
+        </CopyrightItem>
+      </CopyrightWrapper>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ components/layout/footer/FooterStyles.js
@@ -0,0 +1,244 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+  width: 100%;
+  height: auto;
+  padding: 5rem 10rem;
+
+  background-color: ${(props) => props.theme.colors.secondary};
+
+  display: grid;
+  grid-template-columns: repeat(2, minmax(min-content, 1fr));
+  grid-template-rows: repeat(3, min-content);
+  grid-template-areas:
+    "lo nl"
+    "ad li"
+    "co co";
+
+  align-items: start;
+  gap: 2rem;
+`;
+
+const Logo = styled.img`
+  grid-area: lo;
+  width: 10rem;
+  height: 10rem;
+`;
+
+const Newsletter = styled.div`
+  grid-area: nl;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, min-content);
+  align-items: center;
+  justify-items: start;
+  gap: 1rem;
+`;
+
+const NewsletterTitle = styled.p`
+  font-size: 1.4rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  color: ${(props) => props.theme.colors.white};
+  text-align: left;
+`;
+
+const InputWrapper = styled.form`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 80% 1fr;
+  gap: 1rem;
+  align-items: center;
+  justify-items: start;
+`;
+
+const InputFieldWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-left: 1px solid rgba(255, 255, 255, 0.4);
+  border-right: 1px solid rgba(255, 255, 255, 0.4);
+`;
+
+const NewsletterInput = styled.input`
+  outline: none;
+  border: none;
+  width: 100%;
+  height: 100%;
+  padding: 1rem 2rem;
+
+  font-size: 1.6rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  color: ${(props) => props.theme.colors.white};
+  background-color: transparent;
+`;
+
+const NewsletterButton = styled.button`
+  outline: none;
+  border: none;
+  background-color: transparent;
+  font-size: 1.8rem;
+  padding: 1rem;
+  color: ${(props) => props.theme.colors.secondaryLight};
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    svg {
+      transform: translateX(4px);
+    }
+  }
+
+  & > svg {
+    width: 2rem;
+    height: 2rem;
+    fill: ${(props) => props.theme.colors.secondaryLight};
+    margin-left: 1rem;
+    transition: transform 0.2s ease;
+  }
+`;
+
+const ContactWrapper = styled.div`
+  grid-area: ad;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(min-content, 1fr));
+  align-items: start;
+  justify-items: start;
+  gap: 3rem;
+
+  border-top: 1px solid rgba(255, 255, 255, 0.4);
+  padding: 2rem 0 0 0;
+`;
+
+const ContactGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  & > *:not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const ContactItem = styled.p`
+  font-size: 1.4rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  align-items: left;
+  color: ${(props) => props.theme.colors.white};
+`;
+
+const LinkWrapper = styled.div`
+  grid-area: li;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, min-content);
+  align-items: center;
+  justify-items: end;
+  gap: 1rem;
+
+  border-top: 1px solid rgba(255, 255, 255, 0.4);
+  padding: 2rem 0 0 0;
+`;
+
+const SocialMediaGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  & > *:not(:last-child) {
+    margin-right: 2rem;
+  }
+`;
+
+const SocialIcon = styled.svg`
+  width: 2.5rem;
+  height: 2.5rem;
+  fill: ${(props) => props.theme.colors.white};
+  transition: transform 0.2s ease, fill 0.2s ease;
+
+  &:hover {
+    transform: rotate(2deg);
+    fill: ${(props) => props.theme.colors.secondaryLight};
+  }
+`;
+
+const LinkGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  & > *:not(:last-child) {
+    margin-right: 2rem;
+  }
+`;
+
+const LinkItem = styled.a`
+  font-size: 1.4rem;
+  font-weight: 600;
+  text-decoration: none;
+  outline: none;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.white};
+  transition: transform 0.2s ease, color 0.2s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    color: ${(props) => props.theme.colors.secondaryLight};
+  }
+`;
+
+const CopyrightWrapper = styled.div`
+  grid-column: co;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid rgba(255, 255, 255, 0.4);
+  padding: 2rem 0 0 0;
+`;
+
+const CopyrightItem = styled.p`
+  font-size: 1.4rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  color: ${(props) => props.theme.colors.white};
+
+  & > a {
+    font-weight: 600;
+    color: inherit;
+    outline: none;
+    text-decoration: none;
+    transition: color 0.2s ease;
+
+    &:hover {
+      color: ${(props) => props.theme.colors.primaryLight};
+    }
+  }
+`;
+
+export {
+  Container,
+  Logo,
+  Newsletter,
+  NewsletterTitle,
+  InputWrapper,
+  InputFieldWrapper,
+  NewsletterInput,
+  NewsletterButton,
+  ContactWrapper,
+  ContactGroup,
+  ContactItem,
+  LinkWrapper,
+  SocialMediaGroup,
+  SocialIcon,
+  LinkGroup,
+  LinkItem,
+  CopyrightWrapper,
+  CopyrightItem,
+};
