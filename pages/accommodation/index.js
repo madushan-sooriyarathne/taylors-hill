80120dec915bed52cf927f53720677c13220b3eb
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-17 05:03:45 +0530

    created accommodation folder and index.js inside it for accommodation route

---
+++ pages/accommodation/index.js
@@ -0,0 +1,18 @@
+import Page from "../../components/layout/page/Page";
+import CoverImage from "../../components/layout/cover-image/CoverImage";
+import RoomGroup from "../../components/layout/rooms-group/RoomsGroup";
+
+const Accommodation = () => {
+  return (
+    <Page>
+      <CoverImage
+        image="/static/img/rooms/pattiyagama.jpg"
+        title="Accommodation"
+        subTitle="Best in class rooms with a touch of british architecture"
+      />
+      <RoomGroup />
+    </Page>
+  );
+};
+
+export default Accommodation;
