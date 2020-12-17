242741db514196d1cace8a8740ab69075f0dd43b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-18 02:19:23 +0530

    created [roomName].js for individual rooms

---
+++ pages/accommodation/[roomName].js
@@ -0,0 +1,40 @@
+import { rooms } from "../../site-data";
+import Page from "../../components/layout/page/Page";
+import CoverImage from "../../components/layout/cover-image/CoverImage";
+import RoomPage from "../../components/layout/room-page/RoomPage";
+
+const Room = ({ primaryRoom, otherRooms }) => {
+  return (
+    <Page>
+      <CoverImage
+        image={primaryRoom.image}
+        title={primaryRoom.title}
+        subTitle={primaryRoom.type}
+      />
+      <RoomPage primaryRoom={primaryRoom} otherRooms={otherRooms} />
+    </Page>
+  );
+};
+
+const getStaticProps = async ({ params }) => {
+  const primaryRoom = rooms.filter((room) => room.id === params.roomName)[0];
+  const otherRooms = rooms.filter((room) => room.id !== params.roomName);
+
+  console.log(primaryRoom);
+  return {
+    props: {
+      primaryRoom,
+      otherRooms: otherRooms,
+    },
+  };
+};
+
+const getStaticPaths = async () => {
+  const paths = rooms.map((room) => ({ params: { roomName: room.id } }));
+
+  console.log(paths);
+  return { paths, fallback: false };
+};
+
+export { getStaticProps, getStaticPaths };
+export default Room;
