6fd066e69193816118d19ba850284f19c55c37fa
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-18 15:53:10 +0530

    added alignment prop to alight the heading

--- components/headings/heading-sub/HeadingSub.js
+++ components/headings/heading-sub/HeadingSub.js
@@ -1,7 +1,9 @@
 import { Heading } from "./HeadingSubStyles";
 
-const HeadingSub = ({ color, children }) => (
-  <Heading color={color}>{children}</Heading>
+const HeadingSub = ({ color, children, alignment }) => (
+  <Heading color={color} alignment={alignment}>
+    {children}
+  </Heading>
 );
 
 export default HeadingSub;
--- components/headings/heading-sub/HeadingSubStyles.js
+++ components/headings/heading-sub/HeadingSubStyles.js
@@ -5,9 +5,8 @@ const Heading = styled.h3`
   font-weight: 300;
   font-family: ${(props) => props.theme.fonts.primary};
   letter-spacing: 1px;
-  color: ${(props) =>
-    props.color ? props.color : props.theme.colors.secondary};
-  text-align: center;
+  color: ${(props) => props.color || props.theme.colors.secondary};
+  text-align: ${(props) => props.alignment || "center"};
 `;
 
 export { Heading };
