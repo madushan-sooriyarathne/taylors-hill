05ebc434b3c5ce1096f6cf9c056fc854e2c4743c
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-18 22:36:54 +0530

    creatd paragraph component

---
+++ components/paragraph/Paragraph.js
@@ -0,0 +1,7 @@
+import { Para } from "./ParagraphStyles";
+
+const Paragraph = ({ children }) => {
+  return <Para>{children}</Para>;
+};
+
+export default Paragraph;
---
+++ components/paragraph/ParagraphStyles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Para = styled.p`
+  font-size: 1.5rem;
+  font-weight: 400;
+  letter-spacing: 1px;
+  line-height: 2rem;
+  text-align: ${(props) => props.alignment || "left"};
+  color: ${(props) => props.theme.colors.secondary};
+
+  & > b,
+  & > strong {
+    font-weight: 700;
+  }
+
+  & > i,
+  & > em {
+    font-style: italic;
+  }
+
+  & > small {
+    font-size: 1.3rem;
+  }
+
+  & > mark {
+    background-color: ${(props) => props.theme.colors.bgDark};
+  }
+`;
+
+export { Para };
