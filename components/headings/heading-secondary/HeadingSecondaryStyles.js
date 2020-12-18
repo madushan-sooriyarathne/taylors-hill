1b1726765c7b20ea16785605592fe63467340c54
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-18 22:34:18 +0530

    chnaged the letter spaciing from 2px to 1px in heading secondary

--- components/headings/heading-secondary/HeadingSecondaryStyles.js
+++ components/headings/heading-secondary/HeadingSecondaryStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Heading = styled.h2`
   font-size: 2.5rem;
   font-weight: 700;
-  letter-spacing: 2px;
+  letter-spacing: 1px;
   text-align: ${(props) => props.alignment || "center"};
   font-family: ${(props) => props.theme.fonts.primary};
   color: ${(props) => props.color || props.theme.colors.secondary};
