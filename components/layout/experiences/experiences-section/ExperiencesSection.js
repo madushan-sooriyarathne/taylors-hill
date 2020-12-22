e79d00f4ff19ba09b4794bb92a162a9d229d953c
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-22 21:47:51 +0530

    renamed to experiencesSection

---
+++ components/layout/experiences/experiences-section/ExperiencesSection.js
@@ -0,0 +1,51 @@
+import HeadingSecondary from "../../../headings/heading-secondary/HeadingSecondary";
+import MainTitle from "../../main-title/MainTitle";
+import Button from "../../../buttons/button/Button";
+import ImageBox from "../../../image-box/ImageBox";
+
+import {
+  Section,
+  ContentWrapper,
+  Content,
+  ContentItem,
+  Description,
+} from "./ExperiencesSectionStyles";
+
+const ExperiencesSection = ({ experiences }) => {
+  return (
+    <Section>
+      <MainTitle
+        mainHeading="Experience"
+        subHeading="Things we offer that keep you occupied"
+        shadowHeading="Things to do"
+      />
+      <ContentWrapper>
+        {experiences.map((item, index) => {
+          if (index < 3) return <ImageBox {...item} key={item.id} withButton />;
+        })}
+        <Content>
+          <ContentItem>
+            <HeadingSecondary>Activities</HeadingSecondary>
+            <Description>
+              Take time to relax, enjoy and explore! There’s enough to keep
+              guests occupied with the variety of activates that Taylors Hill
+              offers within the premises.
+            </Description>
+          </ContentItem>
+          <ContentItem>
+            <HeadingSecondary>Excursions</HeadingSecondary>
+            <Description>
+              A stay would not be completed without having explored the various
+              attractions that are within short distance of Taylors Hill.
+            </Description>
+          </ContentItem>
+          <Button route="/experience" invert>
+            Explore
+          </Button>
+        </Content>
+      </ContentWrapper>
+    </Section>
+  );
+};
+
+export default ExperiencesSection;
