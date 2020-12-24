d6ee32a6c99b934664765941b9aa74aaf68a9b74
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-25 03:05:57 +0530

    created contact page

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Page from "../components/layout/page/Page";
+import ContactPage from "../components/layout/contact-page/ContactPage";
+
+const Contact = () => {
+  return (
+    <Page compressedHeader>
+      <ContactPage />
+    </Page>
+  );
+};
+
+export default Contact;
