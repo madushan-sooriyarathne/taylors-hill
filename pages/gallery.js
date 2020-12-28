78a117425faf4cae861bd41a489d575ebc849466
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-12-28 05:36:20 +0530

    created gallery route by adding gallery.js file

---
+++ pages/gallery.js
@@ -0,0 +1,12 @@
+import Page from "../components/layout/page/Page";
+import GalleryPage from "../components/layout/gallery/gallery-page/GalleryPage";
+
+const Gallery = () => {
+  return (
+    <Page compressedHeader>
+      <GalleryPage />
+    </Page>
+  );
+};
+
+export default Gallery;
