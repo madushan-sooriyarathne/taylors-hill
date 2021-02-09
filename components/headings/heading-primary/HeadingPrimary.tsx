f1f50f5178ab212e8f649dbe8a5a3e2bdaea71c5
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-09 22:38:22 +0530

    converted PrimaryHeading component & it's styles to typescript

---
+++ components/headings/heading-primary/HeadingPrimary.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { Heading } from "./HeadingPrimaryStyles";
+
+interface Props {
+  children: ReactNode;
+  color: string;
+  alignment?: "left" | "right" | "center";
+}
+
+const HeadingMain: React.FC<Props> = ({
+  children,
+  color,
+  alignment = "center",
+}: Props): JSX.Element => {
+  return (
+    <Heading color={color} alignment={alignment}>
+      {children}
+    </Heading>
+  );
+};
+
+export default HeadingMain;
--- components/headings/heading-primary/HeadingPrimaryStyles.ts
+++ components/headings/heading-primary/HeadingPrimaryStyles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-const Heading = styled.h1`
+interface HeadingProps {
+  alignment: "center" | "left" | "right";
+}
+
+const Heading = styled.h1<HeadingProps>`
   font-size: 5rem;
   font-weight: 900;
-  text-align: ${(props) => props.alignment || "center"};
+  text-align: ${(props) => props.alignment};
   font-family: ${(props) => props.theme.fonts.secondary};
   color: ${(props) =>
     props.color ? props.color : props.theme.colors.secondary};
