f17417480de79e9292b32580afa194f8911bf10e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-09 22:25:43 +0530

    converted Button component & it's styles to typescript

---
+++ components/buttons/button/Button.tsx
@@ -0,0 +1,41 @@
+import { NextRouter, useRouter } from "next/router";
+import { ReactNode, MouseEvent } from "react";
+import { Btn } from "./ButtonStyles";
+
+interface RequiredProps {
+  children: ReactNode;
+  invert?: boolean;
+}
+
+interface OptionalPropsOne {
+  route: string;
+  onClick?: (event?: MouseEvent<HTMLButtonElement>) => void;
+}
+
+interface OptionalPropsTwo {
+  route?: string;
+  onClick: (event?: MouseEvent<HTMLButtonElement>) => void;
+}
+
+type Props = RequiredProps & (OptionalPropsOne | OptionalPropsTwo);
+
+const Button: React.FC<Props> = ({
+  route,
+  onClick,
+  children,
+  invert = false,
+}: Props): JSX.Element => {
+  const router: NextRouter = useRouter();
+
+  function handleClick(): void {
+    router.push(route);
+  }
+
+  return (
+    <Btn onClick={onClick || handleClick} invert={invert}>
+      <span>{children}</span>
+    </Btn>
+  );
+};
+
+export default Button;
--- components/buttons/button/ButtonStyles.ts
+++ components/buttons/button/ButtonStyles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-const Btn = styled.button`
+interface BtnProps {
+  invert: boolean;
+}
+
+const Btn = styled.button<BtnProps>`
   position: relative;
   display: flex;
   align-items: center;
