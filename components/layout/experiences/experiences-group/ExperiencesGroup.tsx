bb3dd79b1e9f13d2ee77126fc0629930bfc31e52
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-14 03:22:32 +0530

    converted ExperiencesGroup component to typescript

--- components/layout/experiences/experiences-group/ExperiencesGroup.tsx
+++ components/layout/experiences/experiences-group/ExperiencesGroup.tsx
@@ -2,17 +2,26 @@ import { Group, Section } from "./ExperiencesGroupStyles";
 import ImageBox from "../../../image-box/ImageBox";
 import MainTitle from "../../main-title/MainTitle";
 
-const ExperiencesGroup = ({ experiences, mainHeading, subHeading }) => {
+interface Props {
+  experiences: Excursion[];
+  mainHeading: string;
+  subHeading: string;
+}
+
+const ExperiencesGroup: React.FC<Props> = ({
+  experiences,
+  mainHeading,
+  subHeading,
+}: Props): JSX.Element => {
   return (
     <Section>
       <MainTitle
         mainHeading={mainHeading}
         subHeading={subHeading}
         shadowHeading={mainHeading}
-        alignment="left"
       />
       <Group>
-        {experiences.map((item) => (
+        {experiences.map((item: Excursion) => (
           <ImageBox key={item.id} {...item} withButton />
         ))}
       </Group>
