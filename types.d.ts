43dc93ba5c80d9d5addb4f25e69695c364b776b8
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-02-14 03:16:21 +0530

    added some new types for room, excursions, offers and galleryImages

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,92 @@
 declare global {
-  type AlignmentProps = "center" | "left" | "right";
+  type AlignmentProps = "center" | "left" | "right" | "justify";
+
+  interface Offer {
+    title: string;
+    image: string;
+    description: string;
+    id: string;
+  }
+
+  interface Room {
+    id: string;
+    title: string;
+    type: string;
+    summery: {
+      title: string;
+      data: string;
+    }[];
+    features: string[];
+    image: string;
+    images: string[];
+    intro: string;
+    description: string;
+  }
+
+  interface Excursion {
+    id: string;
+    image: string;
+    images: string[];
+    title: string;
+    url: string;
+    summery: string;
+    description: string;
+  }
+
+  interface Offer {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    currency: "LKR" | "USD";
+    price: number | null;
+    discount: number | null;
+    validTill: Date;
+    terms: string[];
+    includes: string[];
+  }
+
+  interface HeroSlides {
+    id: number;
+    image: string;
+    text: {
+      heading: string;
+      description: string;
+    };
+    textPos: CSSGridPos;
+  }
+
+  interface SectionData {
+    image: string;
+    mainHeading: string;
+    subHeading: string;
+    shadowHeading: string;
+    route: { url: string; name: string } | null;
+    description: string;
+  }
+
+  interface GalleryImage {
+    img: string;
+    type: string;
+    id: number;
+  }
+
+  interface GallerySelection {
+    id: string;
+    name: string;
+  }
+
+  type CSSStyles = { [key: string]: string };
+  type CSSGridPos = {
+    column: {
+      start: number;
+      end: number;
+    };
+    row: {
+      start: number;
+      end: number;
+    };
+  };
 }
 
 export {};
