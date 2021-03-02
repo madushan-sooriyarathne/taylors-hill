0a3cb94d7cb743cdb6728af64162bb99aa25adc1
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-03 00:12:50 +0530

    completed the OverlayMenu component with nav links and animations

--- components/layout/overlay-menu/OverlayMenu.tsx
+++ components/layout/overlay-menu/OverlayMenu.tsx
@@ -1,24 +1,54 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
+import { NextRouter, useRouter } from "next/router";
+
 import {
   MenuOpenContext,
   MenuOpenDispatchContext,
 } from "../../../context/MenuContext";
 import Button from "../../buttons/button/Button";
 
-import { MenuOverlay, CloseButtonWrapper } from "./OverlayMenuStyles";
+import {
+  MenuOverlay,
+  CloseButtonWrapper,
+  NavLinkGroup,
+  NavLink,
+} from "./OverlayMenuStyles";
+
+import { navLinks } from "../../../site-data";
+import { useTrail } from "react-spring";
 
 const OverlayMenu: React.FC = (): JSX.Element => {
+  const router: NextRouter = useRouter();
+
   // context subscribers
   const menuOpen: boolean = useContext<boolean>(MenuOpenContext);
   const setMenuOpen: DispatchFn = useContext<DispatchFn | null>(
     MenuOpenDispatchContext
   ) as DispatchFn;
 
+  // Nav Links transition
+  const navLinksAnimationTrail = useTrail(navLinks.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    transform: menuOpen ? "translateX(0)" : "translateX(-5px)",
+    opacity: menuOpen ? 1 : 0,
+    from: { opacity: 0, transform: "translateX(-5px)" },
+    delay: 500,
+  });
+
   // Menu button click event
   const closeMenu = (): void => {
     document.body.style.overflowY = "auto";
     setMenuOpen(false);
   };
+
+  const handleNavLinkClick = useCallback((route: string): void => {
+    // close the menu after 0.5 sec
+    setTimeout(() => {
+      router.push(route);
+      document.body.style.overflowY = "auto";
+    }, 500);
+    setMenuOpen(false);
+  }, []);
 
   return (
     <MenuOverlay menuOpen={menuOpen}>
@@ -27,6 +57,17 @@ const OverlayMenu: React.FC = (): JSX.Element => {
           Close
         </Button>
       </CloseButtonWrapper>
+      <NavLinkGroup>
+        {navLinksAnimationTrail.map((props, index) => (
+          <NavLink
+            key={navLinks[index].id}
+            style={props}
+            onClick={() => handleNavLinkClick(navLinks[index].route)}
+          >
+            {navLinks[index].name}
+          </NavLink>
+        ))}
+      </NavLinkGroup>
     </MenuOverlay>
   );
 };
--- components/layout/overlay-menu/OverlayMenuStyles.ts
+++ components/layout/overlay-menu/OverlayMenuStyles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { animated } from "react-spring";
 
 interface MenuOverlayProps {
   menuOpen: boolean;
@@ -16,6 +17,8 @@ const MenuOverlay = styled.div<MenuOverlayProps>`
   overflow-y: auto;
   overflow-x: hidden;
   display: none;
+  align-items: center;
+  justify-content: flex-start;
 
   background: linear-gradient(
       90deg,
@@ -39,7 +42,34 @@ const MenuOverlay = styled.div<MenuOverlayProps>`
     props.menuOpen ? "translateY(0)" : "translateY(-100%)"};
 
   ${(props) => props.theme.responsive.mediumLarge} {
-    display: inline-block;
+    display: flex;
+  }
+`;
+
+const NavLinkGroup = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  padding: 5rem;
+
+  & > *:not(:last-child) {
+    margin-bottom: 3rem;
+  }
+`;
+
+const NavLink = styled(animated.a)`
+  font-size: 4rem;
+  font-weight: 700;
+  letter-spacing: 1px;
+  color: ${(props) => props.theme.colors.secondary};
+  text-decoration: none;
+  outline: none;
+
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.secondaryLight};
   }
 `;
 
@@ -49,4 +79,4 @@ const CloseButtonWrapper = styled.div`
   right: 5rem;
 `;
 
-export { MenuOverlay, CloseButtonWrapper };
+export { MenuOverlay, CloseButtonWrapper, NavLinkGroup, NavLink };
