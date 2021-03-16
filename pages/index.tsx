eb5b7700248df9b452fb3239ddc0558a9eadb0e5
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-16 20:10:33 +0530

    added getStaticProps function fetching data from contentful

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import {
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+} from "next";
+import {
+  getMultipleEntries,
+  getSingleEntry,
+  serializeAssetUrls,
+} from "../utils/contentful";
+
 import CTADescription from "../components/layout/cta-description/CTADescription";
 import RoomsSection from "../components/layout/rooms/rooms-section/RoomsSection";
 import ExperiencesSection from "../components/layout/experiences/experiences-section/ExperiencesSection";
@@ -6,17 +17,25 @@ import ImageContentSection from "../components/layout/Image-content-section/Imag
 import Page from "../components/layout/page/Page";
 import OfferSection from "../components/layout/offers/offer-section/OfferSection";
 import Slider from "../components/layout/slider/Slider";
-import {
-  rooms,
-  ourStory,
-  dining,
-  offers,
-  slides,
-  excursions,
-  galleryImages,
-} from "../site-data";
+import { galleryImages } from "../site-data";
 
-const Home: React.FC = (): JSX.Element => {
+interface Props {
+  slides: HeroSlide[];
+  ourStory: SectionData;
+  rooms: Room[];
+  experiences: Excursion[];
+  dining: SectionData;
+  offers: Offer[];
+}
+
+const Home: React.FC<Props> = ({
+  ourStory,
+  rooms,
+  experiences,
+  dining,
+  slides,
+  offers,
+}: Props): JSX.Element => {
   return (
     <Page>
       <Slider slides={slides} />
@@ -24,7 +43,7 @@ const Home: React.FC = (): JSX.Element => {
         <CTADescription data={ourStory} />
       </ImageContentSection>
       <RoomsSection rooms={rooms} />
-      <ExperiencesSection experiences={excursions} />
+      <ExperiencesSection experiences={experiences} />
       <ImageContentSection image={dining.image} reversed>
         <CTADescription data={dining} />
       </ImageContentSection>
@@ -34,4 +53,76 @@ const Home: React.FC = (): JSX.Element => {
   );
 };
 
+const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<Props>
+> => {
+  // Fetching hero slider image data
+  const slides: HeroSlide[] = await getMultipleEntries<ContentfulHeroSlideFields>(
+    "heroSlide"
+  ).then((result) => {
+    return result.map((slide) =>
+      serializeAssetUrls<ContentfulHeroSlideFields, HeroSlide>(slide, "image")
+    );
+  });
+
+  // Fetching Our story section data
+  const ourStory: SectionData = serializeAssetUrls<
+    ContentfulSectionDataFields,
+    SectionData
+  >(
+    await getSingleEntry<ContentfulSectionDataFields>("1coWWPz6KIqAadTQ0JJzEn"),
+    "image"
+  );
+
+  // Fetching Room Data
+  const rooms: Room[] = await getMultipleEntries<ContentfulRoomFields>(
+    "room"
+  ).then((result) => {
+    return result.map((item) =>
+      serializeAssetUrls<ContentfulRoomFields, Room>(item, "image", "images")
+    );
+  });
+
+  // Fetch Activities & Excursion data
+  const excursions: ContentfulExcursionFields[] = await getMultipleEntries<ContentfulExcursionFields>(
+    "excursion"
+  );
+  const activities: ContentfulExcursionFields[] = await getMultipleEntries<ContentfulExcursionFields>(
+    "activity"
+  );
+
+  const experiences: Excursion[] = [...excursions, ...activities].map((item) =>
+    serializeAssetUrls<ContentfulExcursionFields, Excursion>(
+      item,
+      "image",
+      "images"
+    )
+  );
+
+  // Fetching Dining section data
+  const dining: SectionData = serializeAssetUrls<
+    ContentfulSectionDataFields,
+    SectionData
+  >(
+    await getSingleEntry<ContentfulSectionDataFields>("1Z0W518MDLc43EjjP4eAP8"),
+    "image"
+  );
+
+  // Fetch Offer data
+  const offers: Offer[] = await getMultipleEntries<ContentfulOfferFields>(
+    "offer"
+  ).then((result) => {
+    return result.map((item) =>
+      serializeAssetUrls<ContentfulOfferFields, Offer>(item, "image")
+    );
+  });
+
+  // // TODO: Fetch Gallery Images
+
+  return {
+    props: { slides, ourStory, rooms, experiences, dining, offers },
+  };
+};
+
+export { getStaticProps };
 export default Home;
