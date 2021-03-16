81cf43464b7610e74b7b03ecd5f3258a400f2f74
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-16 20:11:22 +0530

    added getStaticProps function to Offer Index page

--- pages/offers/index.tsx
+++ pages/offers/index.tsx
@@ -2,9 +2,15 @@ import Page from "../../components/layout/page/Page";
 import CoverImage from "../../components/layout/cover-image/CoverImage";
 import OfferGroup from "../../components/layout/offers/offer-group/OfferGroup";
 
-import { offers } from "../../site-data";
+import NoOfferMessage from "../../components/layout/no-offers-message/NoOffersMessage";
+import { GetStaticProps, GetStaticPropsResult } from "next";
+import { getMultipleEntries } from "../../utils/contentful";
 
-const Offers: React.FC = (): JSX.Element => {
+interface Props {
+  offers: Offer[];
+}
+
+const Offers: React.FC<Props> = ({ offers }: Props): JSX.Element => {
   return (
     <Page>
       <CoverImage
@@ -12,9 +18,34 @@ const Offers: React.FC = (): JSX.Element => {
         subTitle="Grab the best deals for your stay"
         image="/static/img/offers-cover.webp"
       />
-      <OfferGroup offers={offers} />
+
+      {offers.length < 1 ? <NoOfferMessage /> : <OfferGroup offers={offers} />}
     </Page>
   );
 };
 
+const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<Props>
+> => {
+  // Fetch Offers from contentful;
+  const fetchedOffers: ContentfulOfferFields[] = await getMultipleEntries<ContentfulOfferFields>(
+    "offer"
+  );
+
+  // didn't use serializeAssetUrls function here because validTill field also need to be serialized
+  let offers: Offer[] = fetchedOffers.map((offer) => {
+    const { image, validTill, ...rest } = offer;
+    return {
+      ...rest,
+      validTill,
+      image: image.fields.file.url,
+    };
+  });
+
+  return {
+    props: { offers },
+  };
+};
+
+export { getStaticProps };
 export default Offers;
