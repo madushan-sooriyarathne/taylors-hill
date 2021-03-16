09bd0d6222afbd95e0d1f6ca4e54ad3a917e7d0f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-17 00:36:58 +0530

    reversed id to url

--- components/layout/experiences/experiences-section/ExperiencesSection.tsx
+++ components/layout/experiences/experiences-section/ExperiencesSection.tsx
@@ -33,7 +33,7 @@ const ExperiencesSection: React.FC<Props> = ({
               <ImageBox
                 image={item.image}
                 title={item.title}
-                id={item.id}
+                url={`/experience/${item.id}`}
                 key={item.id}
                 withButton
               />
