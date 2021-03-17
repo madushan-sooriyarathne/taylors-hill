0887225f321b18788b81128df8ecbc8ad39b98df
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-17 17:39:31 +0530

    added necessary envrionment vaibales

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ module.exports = {
       process.env.CONTENTFUL_CONTENT_DELIVERY_API_TOKEN,
     CONTENTFUL_CONTENT_PREVIEW_API_TOKEN:
       process.env.CONTENTFUL_CONTENT_PREVIEW_API_TOKEN,
+    EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+    MAILCHIMP_SERVER_PREFIX: process.env.MAILCHIMP_SERVER_PREFIX,
+    MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
