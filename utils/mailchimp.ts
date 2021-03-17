c14f86f6dd83438b5af2abf7210398103f974b12
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-17 14:59:19 +0530

    added types

--- utils/mailchimp.ts
+++ utils/mailchimp.ts
@@ -6,30 +6,27 @@ mailchimp.setConfig({
   server: process.env.MAILCHIMP_SERVER_PREFIX as string,
 });
 
-async function addContact(
-  email: string,
-  firstName?: string,
-  lastName?: string,
-  phone?: string
-): Promise<void> {
+async function addContact(contact: MailchimpContact): Promise<boolean> {
   const listId: string = process.env.MAILCHIMP_LIST_ID as string;
 
   try {
     const response = await mailchimp.lists.addListMember(listId, {
-      email_address: email,
+      email_address: contact.email,
       status: "subscribed",
       merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
-        PHONE: phone,
+        FNAME: contact.firstName,
+        LNAME: contact.lastName,
+        PHONE: contact.phone,
       },
     });
 
     console.log(`user subscribed (${response.id})`);
+    return true;
   } catch (error: any) {
     console.error(
       `Error occurred while subscribing the user - ${error.message}`
     );
+    return false;
   }
 }
 
