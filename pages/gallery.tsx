5fa6007eecac43cfbd5587d0781ed526de29ca13
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-03-19 02:27:09 +0530

    add getStaticProps function

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,16 +1,42 @@
 import Page from "../components/layout/page/Page";
 import GalleryPage from "../components/layout/gallery/gallery-page/GalleryPage";
-import { galleryImages, gallerySelections } from "../site-data";
+import { gallerySelections } from "../site-data";
+import { GetStaticProps, GetStaticPropsResult } from "next";
+import { getMultipleEntries, serializeAssetUrls } from "../utils/contentful";
 
-const Gallery: React.FC = (): JSX.Element => {
+interface Props {
+  images: GalleryImage[];
+}
+
+const Gallery: React.FC<Props> = ({ images }: Props): JSX.Element => {
   return (
     <Page compressedHeader>
-      <GalleryPage
-        images={galleryImages}
-        gallerySelections={gallerySelections}
-      />
+      <GalleryPage images={images} gallerySelections={gallerySelections} />
     </Page>
   );
 };
 
+const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<Props>
+> => {
+  // fetch images and serialize image urls
+  const images: GalleryImage[] = await getMultipleEntries<ContentfulGalleryImageFields>(
+    "galleryImage"
+  ).then((result: ContentfulGalleryImageFields[]) =>
+    result.map((item: ContentfulGalleryImageFields) =>
+      serializeAssetUrls<ContentfulGalleryImageFields, GalleryImage>(
+        item,
+        "image"
+      )
+    )
+  );
+
+  return {
+    props: {
+      images,
+    },
+  };
+};
+
+export { getStaticProps };
 export default Gallery;
