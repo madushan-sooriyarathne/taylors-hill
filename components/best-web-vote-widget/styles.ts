4b9522208ff57a974f601fcf0bdb6fa43344ba21
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2024-06-18 21:51:35 +0530

    fix: fix best web voting widget

--- components/best-web-vote-widget/styles.ts
+++ components/best-web-vote-widget/styles.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const Widget = styled.a`
   position: fixed;
   bottom: 0;
-  left: 4rem;
+  left: 2rem;
   background-color: ${(props) => props.theme.colors.white};
-  padding: 1rem;
+  padding-bottom: 1rem;
   height: auto;
   width: auto;
   text-decoration: none;
@@ -13,7 +13,6 @@ const Widget = styled.a`
   flex-direction: column;
   align-items: center;
   justify-content: start;
-  row-gap: 0.5rem;
   border-top-left-radius: 2px;
   border-top-right-radius: 2px;
   z-index: 290;
@@ -22,6 +21,7 @@ const Widget = styled.a`
   & > div {
     width: 10rem;
     height: 10rem;
+    scale: 1.2;
   }
 
   & > span {
